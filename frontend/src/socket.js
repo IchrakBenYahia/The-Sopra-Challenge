7ad37c1c65cd8f3bd4d852e8bec19b0ee52a74b1
IchrakBenYahia <ichrakbenyahia2001@egmail.com>
2025-12-25 10:32:05 +0100

    malek+amal+ichrak

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,7 @@
 import { io } from 'socket.io-client';
 
 // REMPLACEZ '192.168.1.25' PAR VOTRE VRAIE ADRESSE IP TROUVÉE À L'ÉTAPE 2
-const URL = "http://192.168.0.33:3001"; 
+const URL = "http://localhost:3001"; 
 
 export const socket = io(URL, {
   autoConnect: true
